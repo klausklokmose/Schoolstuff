47798c3ea07bfc1b70a49d2456cbffed591dc8d2
Klaus Klokmose Nielsen <kkni11@student.aau.dk>
2015-02-19 16:58:43 +0100

    classification and frequent patter mining

--- Lab 3 - Classification/src/Main.java
+++ Lab 3 - Classification/src/Main.java
@@ -19,85 +19,109 @@ public class Main {
 	public static void main(String[] args) {
 		// First step - Load data and convert to Mushroom objects.
 		ArrayList<Mushroom> mushrooms = DataManager.LoadData();
-		System.out.println("DataManager loaded "+mushrooms.size() + " mushrooms");
-		int trainingSetSize = 1000;
-		int testSetSize = mushrooms.size()-trainingSetSize;
-		int k = 200;
-		
+		System.out.println("DataManager loaded " + mushrooms.size()
+				+ " mushrooms");
+		int trainingSetSize = 2000;
+		int testSetSize = mushrooms.size() - trainingSetSize;
+		int TO = 300;
+for (int k = 1; k < TO; k++) {
+	
 		List<Mushroom> trainingSet = new ArrayList<Mushroom>();
-		for (int i = 0; i < mushrooms.size(); i++) {
+		for (int i = 0; i < trainingSetSize; i++) {
 			trainingSet.add(mushrooms.get(i));
 		}
-		MushroomClassification di = null;
 		List<MushroomClassification> classifications = new ArrayList<>();
-		for (int i = testSetSize; i < mushrooms.size(); i++) {
-			List<MushroomClassification> distances = new ArrayList<MushroomClassification>();
-			getDistancesFromI(mushrooms, trainingSetSize, trainingSet, i,
-					distances);
-			List<MushroomClassification> kNearest = getKNearest(k, distances);
-			int numPoisonous = getNumPoisonous(kNearest);
-			int numEdible = Math.abs(kNearest.size()-numPoisonous);
-			Class_Label label = (numPoisonous > numEdible) ? Class_Label.poisonous : Class_Label.edible;
-			di = new MushroomClassification(mushrooms.get(i), label);
+		int numGoodClassifications = 0;
+
+		for (int i = trainingSetSize; i < mushrooms.size(); i++) {
+			Mushroom mushr = mushrooms.get(i);
+			
+			Class_Label label = classifyLabel(k, mushr, trainingSet);
+			MushroomClassification di = new MushroomClassification(
+					mushrooms.get(i), label);
 			classifications.add(di);
-			System.out.println("-->"+label.equals(mushrooms.get(i).getAttributeValue(Class_Label.class)));
+			numGoodClassifications += label.equals(mushr
+					.getAttributeValue(Class_Label.class)) ? 1 : 0;
 		}
+
+		System.out.println(""
+				+ (((double) numGoodClassifications / testSetSize) * 100));
+}
+		
+	}
+
+	private static Class_Label classifyLabel(int k, Mushroom mushr,
+			List<Mushroom> trainingSet) {
+		List<MushroomClassification> distances = getDistancesFromI(
+				trainingSet, mushr);
+		List<MushroomClassification> kNearest = getKNearest(k, distances);
+		int numPoisonous = getNumPoisonous(kNearest);
+		int numEdible = Math.abs(kNearest.size() - numPoisonous);
+		Class_Label label = (numPoisonous > numEdible) ? Class_Label.poisonous
+				: Class_Label.edible;
+		return label;
 	}
 
 	private static int getNumPoisonous(List<MushroomClassification> kNearest) {
 		int num = 0;
 		for (MushroomClassification mushroomDist : kNearest) {
-			if(mushroomDist.getM().getAttributeValue(Class_Label.class).equals(Class_Label.poisonous)){
+			if (mushroomDist.getM().getAttributeValue(Class_Label.class)
+					.equals(Class_Label.poisonous)) {
 				num++;
 			}
 		}
 		return num;
 	}
 
-	private static List<MushroomClassification> getKNearest(int k, List<MushroomClassification> distances) {
+	private static List<MushroomClassification> getKNearest(int k,
+			List<MushroomClassification> distances) {
 		Collections.sort(distances, new DistComparator());
-
+		if (k > distances.size()) {
+			k = distances.size();
+		}
 		List<MushroomClassification> kNearest = new ArrayList<>();
 		for (int j = 0; j < k; j++) {
 			kNearest.add(distances.get(j));
-//			System.out.print(distances.get(j).getM().getAttributeValue(Class_Label.class)+", ");
+			// System.out.print(distances.get(j).getM().getAttributeValue(Class_Label.class)+", ");
 		}
 		return kNearest;
 	}
 
-	private static void getDistancesFromI(ArrayList<Mushroom> mushrooms,
-			int trainingSetSize, List<Mushroom> trainingSet, int i,
-			List<MushroomClassification> distances) {
-		MushroomClassification di;
-		for (int j = 0; j < trainingSetSize; j++) {
-//				add to priority-queue dist(mushrooms.get(i), trainingSet.get(j));
-			di = new MushroomClassification(mushrooms.get(i), dist(mushrooms.get(i), trainingSet.get(j)));
-			distances.add(di);
+	private static List<MushroomClassification> getDistancesFromI(
+			List<Mushroom> trainingSet, Mushroom mushroom) {
+		List<MushroomClassification> distances = new ArrayList<>();
+		for (int i = 0; i < trainingSet.size(); i++) {
+			distances.add(new MushroomClassification(trainingSet.get(i),
+					hammerDist(mushroom, trainingSet.get(i))));
 		}
-		
+		return distances;
 	}
 
-	private static double dist(Mushroom x1, Mushroom x2){
+	private static int hammerDist(Mushroom x1, Mushroom x2) {
 		int sum = 0;
 		ArrayList<Object> attrList = Mushroom.getAttributeList();
-		
+
 		for (int i = 0; i < attrList.size(); i++) {
-			sum += x1.getAttributeValue(attrList.get(i)).equals(x2.getAttributeValue(attrList.get(i))) ? 0 : 1;
+			sum += x1.getAttributeValue(attrList.get(i)).equals(
+					x2.getAttributeValue(attrList.get(i))) ? 0 : 1;
 		}
-		return Math.sqrt(sum);
+		return sum;
 	}
-	public static class DistComparator implements Comparator<MushroomClassification>{
+
+	public static class DistComparator implements
+			Comparator<MushroomClassification> {
 		@Override
-		public int compare(MushroomClassification thizz, MushroomClassification that) {
-			if(that.getD() > thizz.getD()){
+		public int compare(MushroomClassification thizz,
+				MushroomClassification that) {
+			if (that.getD() > thizz.getD()) {
 				return -1;
-			}else if(that.getD() < thizz.getD()){
+			} else if (that.getD() < thizz.getD()) {
 				return 1;
-			}else{
+			} else {
 				return 0;
 			}
 		}
-		
+
 	}
 
 }

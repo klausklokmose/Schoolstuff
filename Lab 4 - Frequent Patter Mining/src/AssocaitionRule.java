c260631f2d963e6749a5fff8ffc739e268afccce
Klaus Klokmose Nielsen <kkni11@student.aau.dk>
2015-02-21 15:14:05 +0100

    Initial Lab 5 commit

--- Lab 4 - Frequent Patter Mining/src/AssocaitionRule.java
+++ Lab 4 - Frequent Patter Mining/src/AssocaitionRule.java
@@ -1,24 +1,36 @@
+import java.util.Arrays;
+
 
 public class AssocaitionRule {
-	private ItemSet set;
-	private double condidence;
+	private int[] a;
+	private int[] b;
+	private int[] AunionB;
+	private double confidence;
 	private double support;
 	
-	public AssocaitionRule(ItemSet set, double support, double condidence){
-		this.set = set;
+	public AssocaitionRule(int[] a, int[] b, int[] AunionB, double support, double confidence){
+		this.a = a;
+		this.b = b;
 		this.support = support;
-		this.condidence = condidence;
+		this.confidence = confidence;
 	}
 
-	public ItemSet getSet() {
-		return set;
+	public int[] getA() {
+		return a;
 	}
 
+	public int[] getAunionB(){
+		return AunionB;
+	}
 	public double getSupport(){
 		return support;
 	}
 	
-	public double getCondidence() {
-		return condidence;
+	public double getConfidence() {
+		return confidence;
+	}
+	
+	public String toString(){
+		return Arrays.toString(a)+" => "+Arrays.toString(b) + "\tSupport = "+(int)support +" %\tConfidence = "+(int)confidence+" %";
 	}
 }
